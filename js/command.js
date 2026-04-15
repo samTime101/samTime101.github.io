e0492a9ac2662ade519437136907bcc885743f27
samTime101 <srijanregmi44@gmail.com>
2026-04-15 22:03:41 +0545

    feat: add article section

--- js/command.js
+++ js/command.js
@@ -26,15 +26,18 @@ I love computers...
 }
 function lsCommand() {
     if (currentDir === '~') {
-        printOutput(`<span style="color:#729fcf;font-weight:bold;">projects</span>  <span style="color:#729fcf;font-weight:bold;">contacts</span>  <span style="color:#729fcf;font-weight:bold;">about</span>`);
+        printOutput(`<span style="color:#729fcf;font-weight:bold;">projects</span>  <span style="color:#729fcf;font-weight:bold;">contacts</span>  <span style="color:#729fcf;font-weight:bold;">about</span>  <span style="color:#729fcf;font-weight:bold;">articles</span>`);
     } else if (currentDir === '~/contacts') {
         printOutput(`<span style="color:#8ae234;font-weight:bold;">email</span>  <span style="color:#8ae234;font-weight:bold;">linkedin</span>  <span style="color:#8ae234;font-weight:bold;">github</span>`);
     }
     else if (currentDir == '~/projects') {
-        printOutput(`<span style="color:white;font-weight:bold;">Currently in development</span>`);
+        printOutput(`<span style="color:white;font-weight:bold;">Projects is currently in development</span>`);
     }
     else if (currentDir == '~/about') {
         printOutput(`<span style="color:white;font-weight:bold;">I am a 19 year old computer science student from Nepal. I like coding and learning new things :)</span>`);
+    }
+    else if (currentDir == '~/articles') {
+        articleCommand();
     }
 }
 
@@ -48,7 +51,7 @@ function cdCommand(dir) {
             currentDir = parts.join('/') || '~';
         }
     } else if (currentDir === '~') {
-        if (dir === 'contacts' || dir === 'projects' || dir === 'about') {
+        if (dir === 'contacts' || dir === 'projects' || dir === 'about' || dir === 'articles') {
             currentDir = `~/${dir === 'about' ? 'about' : dir}`;
         } else {
             printOutput(`time101: cd: ${dir}: No such file or directory`);
@@ -92,4 +95,21 @@ function linkedinCommand() {
 
 function githubCommand() {
     openLink(`Opening github...`, "https://github.com/samTime101", true);
+}
+
+function articleCommand() {
+    const modalEl = document.getElementById('articleModal');
+
+    if (!modalEl) {
+        printOutput('article: modal element not found');
+        return;
+    }
+
+    if (typeof bootstrap === 'undefined' || !bootstrap.Modal) {
+        printOutput('article: bootstrap modal is not available');
+        return;
+    }
+
+    const modal = bootstrap.Modal.getOrCreateInstance(modalEl);
+    modal.show();
 }
